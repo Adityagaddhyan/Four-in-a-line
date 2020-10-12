51904c9366e0b4fb10482b2b081bf4443be4bd68
adityagaddhyan <adityagaddhyan@gmail.com>
2020-10-13 01:36:02 +0530

    removed bad fade in circle

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     function getMousePosition(canvas, event) {
-//         let rect = canvas.getBoundingClientRect();
-//         let x = event.clientX - rect.left;
-//         let y = event.clientY - rect.top;
-//         console.log("Coordinate x: " + x,
-//             "Coordinate y: " + y);
-//         var col = Math.floor(x / 100 + 1);
-//         console.log("col : ",col)
-//     }
-
-//     let canvasElem = document.querySelector("canvas");
-
-//     canvasElem.addEventListener("mousedown", function (e) {
-//         getMousePosition(canvasElem, e);
-//     });
-// });
-// module.exports=getMousePosition(canvasElem,e)
-
-// module.exports=function(){
-//     console.log("concole");
-// }
-
 var arr = [0,0,0,0,0,0,0];
 var movenumber=0;
 function makemove(col) {
@@ -70,7 +47,6 @@ window.onload = function () {
         var y=630+90-row*90-45;
         var myCircle = new Path.Circle(new Point(x,y), 35);
         myCircle.fillColor = player==0?'#40c4ff':'#76ff03';
-        myCircle.fadeIn(4000);
     }
    
     tool.onMouseDrag = function (event) {
